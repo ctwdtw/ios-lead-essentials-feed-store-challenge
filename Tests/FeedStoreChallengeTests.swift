2bc50d8ce94e2b0000b0d9f44d5184de5cd4f08a
Paul Lee <ctwdtw@gmail.com>
2020-10-02 15:11:58 +0800

    use array extension to simplify implementation detail of `insert(_:, timestamp;:completion:)` and `deleteCacheFeed(completion:)`

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -63,7 +63,7 @@ public class RealmFeedStore: FeedStore {
   
   public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
     realmQueue.async(flags: .barrier) {
-      let realmFeed = feed.map { RealmFeedImage(id: $0.id.uuidString, imageDescription: $0.description, location: $0.location, url: $0.url.absoluteString) }
+      let realmFeed = feed.toRealmModels()
       let cache = RealmCache(feed: realmFeed, timestamp: timestamp)
       let realm = try! self.realmFactory()
       try! realm.write {
@@ -79,14 +79,33 @@ public class RealmFeedStore: FeedStore {
     realmQueue.async {
       let realm = try! self.realmFactory()
       guard let cache = realm.objects(RealmCache.self).first else { completion(.empty); return }
-      let localFeed = cache.feed.map { LocalFeedImage(id: UUID(uuidString: $0.id)!, description: $0.imageDescription, location: $0.location, url: URL(string: $0.url)!) }
+      let localFeed = Array(cache.feed).toLocals()
       
-      completion(.found(feed: Array(localFeed), timestamp: cache.timestamp))
+      completion(.found(feed: localFeed, timestamp: cache.timestamp))
     }
   }
   
 }
 
+private extension Array where Element == RealmFeedImage {
+  func toLocals() -> [LocalFeedImage] {
+    let localFeed = map { LocalFeedImage(id: UUID(uuidString: $0.id)!,
+                                         description: $0.imageDescription,
+                                         location: $0.location,
+                                         url: URL(string: $0.url)!) }
+    return localFeed
+  }
+}
+
+private extension Array where Element == LocalFeedImage {
+  func toRealmModels() -> [RealmFeedImage] {
+    let feed = map { RealmFeedImage(id: $0.id.uuidString,
+                                    imageDescription: $0.description,
+                                    location: $0.location,
+                                    url: $0.url.absoluteString) }
+    return feed
+  }
+}
 
 
 class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
