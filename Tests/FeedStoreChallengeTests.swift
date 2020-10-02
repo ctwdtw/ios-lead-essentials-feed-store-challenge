09d949078b4ca01f3bbec10a1b12229e416f792f
Paul Lee <ctwdtw@gmail.com>
2020-10-02 14:50:03 +0800

    delete delivers no error on empty cache

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -50,7 +50,7 @@ public class RealmFeedStore: FeedStore {
   }
   
   public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-    
+    completion(nil)
   }
   
   public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
@@ -158,9 +158,9 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	}
 
 	func test_delete_deliversNoErrorOnEmptyCache() {
-//		let sut = makeSUT()
-//
-//		assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
+		let sut = makeSUT()
+
+		assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
 	}
 
 	func test_delete_hasNoSideEffectsOnEmptyCache() {
