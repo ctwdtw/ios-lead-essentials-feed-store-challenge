d54309e26385a7fd8f8337a339730c6f18b48fe3
Paul Lee <ctwdtw@gmail.com>
2020-10-02 14:12:03 +0800

    use a concurrent queue to handle `FeedStore` operations in background threads.

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -39,10 +39,14 @@ class RealmCache: Object {
 }
 
 public class RealmFeedStore: FeedStore {
-  private let realm: Realm
+  private let realmQueue = DispatchQueue(label: "feedStore.realm.queue", attributes: .concurrent)
   
-  public init(realm: Realm) {
-    self.realm = realm
+  private let realmFactory: RealmFactory
+  
+  public typealias RealmFactory = () throws -> Realm
+  
+  public init(factory: @escaping RealmFactory) {
+    self.realmFactory = factory
   }
   
   public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
@@ -50,19 +54,24 @@ public class RealmFeedStore: FeedStore {
   }
   
   public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-    let realmFeed = feed.map { RealmFeedImage(id: $0.id.uuidString, imageDescription: $0.description, location: $0.location, url: $0.url.absoluteString) }
-    let cache = RealmCache(feed: realmFeed, timestamp: timestamp)
-    
-    try! realm.write { realm.add(cache) }
-    
-    completion(nil)
+    realmQueue.async(flags: .barrier) {
+      let realmFeed = feed.map { RealmFeedImage(id: $0.id.uuidString, imageDescription: $0.description, location: $0.location, url: $0.url.absoluteString) }
+      let cache = RealmCache(feed: realmFeed, timestamp: timestamp)
+      let realm = try! self.realmFactory()
+      try! realm.write { realm.add(cache) }
+  
+      completion(nil)
+    }
   }
   
   public func retrieve(completion: @escaping RetrievalCompletion) {
-    guard let cache = realm.objects(RealmCache.self).first else { completion(.empty); return }
-    let localFeed = cache.feed.map { LocalFeedImage(id: UUID(uuidString: $0.id)!, description: $0.imageDescription, location: $0.location, url: URL(string: $0.url)!) }
-    
-    completion(.found(feed: Array(localFeed), timestamp: cache.timestamp))
+    realmQueue.async {
+      let realm = try! self.realmFactory()
+      guard let cache = realm.objects(RealmCache.self).first else { completion(.empty); return }
+      let localFeed = cache.feed.map { LocalFeedImage(id: UUID(uuidString: $0.id)!, description: $0.imageDescription, location: $0.location, url: URL(string: $0.url)!) }
+      
+      completion(.found(feed: Array(localFeed), timestamp: cache.timestamp))
+    }
   }
   
 }
@@ -82,7 +91,27 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
     //  Repeat this process until all tests are passing.
     //
     //  ***********************
-
+  
+  private var strongRealmReference: Realm?
+  
+  override func setUp() {
+      super.setUp()
+      setupRealmStrongReference()
+  }
+  
+  override func tearDown() {
+      super.tearDown()
+      undoRealmSideEffects()
+  }
+  
+  private func setupRealmStrongReference() {
+      strongRealmReference = try! Realm(configuration: unitTestRealmConfiguration())
+  }
+  
+  private func undoRealmSideEffects() {
+      strongRealmReference = nil
+  }
+  
 	func test_retrieve_deliversEmptyOnEmptyCache() {
 		let sut = makeSUT()
 
@@ -158,9 +187,14 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	// - MARK: Helpers
 	
   private func makeSUT() -> FeedStore {
-    let realm = try! Realm(configuration: Realm.Configuration(inMemoryIdentifier: "UnitTestInMemoryRealm"))
-    let sut = RealmFeedStore(realm: realm)
+    let sut = RealmFeedStore (factory: {
+      return try! Realm(configuration: self.unitTestRealmConfiguration())
+    })
     return sut
+  }
+  
+  private func unitTestRealmConfiguration() -> Realm.Configuration {
+    return Realm.Configuration(inMemoryIdentifier: "UnitTestInMemoryRealm")
   }
 	
 }
