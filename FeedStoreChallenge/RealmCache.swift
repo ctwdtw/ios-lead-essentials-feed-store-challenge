eb700f5949d82a09287d885958baddcb39060d1b
Paul Lee <ctwdtw@gmail.com>
2020-10-02 15:18:39 +0800

    move `RealmFeedStore`, `RealmFeedImage`, `RealmCache` into prod. code

---
+++ FeedStoreChallenge/RealmCache.swift
@@ -0,0 +1,22 @@
+//
+//  RealmCache.swift
+//  FeedStoreChallenge
+//
+//  Created by Paul Lee on 2020/10/2.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmCache: Object {
+  @objc dynamic private(set) var timestamp: Date = Date()
+  let feed = List<RealmFeedImage>()
+  
+  convenience init(feed: [RealmFeedImage], timestamp: Date) {
+    self.init()
+    self.feed.append(objectsIn: feed)
+    self.timestamp = timestamp
+  }
+  
+}
---
+++ FeedStoreChallenge/RealmFeedImage.swift
@@ -0,0 +1,30 @@
+//
+//  RealmFeedImage.swift
+//  FeedStoreChallenge
+//
+//  Created by Paul Lee on 2020/10/2.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmFeedImage: Object {
+  @objc dynamic private(set) var id: String = ""
+  @objc dynamic private(set) var imageDescription: String? = nil
+  @objc dynamic private(set) var location: String? = nil
+  @objc dynamic private(set) var url: String = ""
+  
+  override static func primaryKey() -> String? {
+    return "id"
+  }
+  
+  convenience init(id: String, imageDescription: String?, location: String?, url: String) {
+    self.init()
+    self.id = id
+    self.imageDescription = imageDescription
+    self.location = location
+    self.url = url
+  }
+  
+}
