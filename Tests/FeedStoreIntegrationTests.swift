5b8ba6784671738645bd93cd2e017ffde1f42d4f
Paul Lee <ctwdtw@gmail.com>
2020-10-02 22:19:10 +0800

    implement `makeSUT`, `setupEmptyStoreState` and `undoStoreSideEffects` to make all test cases in `FeedStoreIntegrationTests` passing

--- Tests/FeedStoreIntegrationTests.swift
+++ Tests/FeedStoreIntegrationTests.swift
@@ -8,6 +8,7 @@
 
 import XCTest
 import FeedStoreChallenge
+import RealmSwift
 
 class FeedStoreIntegrationTests: XCTestCase {
 
@@ -31,60 +32,74 @@ class FeedStoreIntegrationTests: XCTestCase {
     }
     
     func test_retrieve_deliversNoEmptyOnEmptyCache() {
-//        let sut = makeSUT()
-//
-//        expect(sut, toRetrieve: .empty)
+        let sut = makeSUT()
+
+        expect(sut, toRetrieve: .empty)
     }
 
     func test_retrieve_deliversFeedInsertedOnAnotherInstance() {
-//        let storeToInsert = makeSUT()
-//        let storeToLoad = makeSUT()
-//        let feed = uniqueImageFeed()
-//        let timestamp = Date()
-//
-//        insert((feed, timestamp), to: storeToInsert)
-//
-//        expect(storeToLoad, toRetrieve: .found(feed: feed, timestamp: timestamp))
+        let storeToInsert = makeSUT()
+        let storeToLoad = makeSUT()
+        let feed = uniqueImageFeed()
+        let timestamp = Date()
+
+        insert((feed, timestamp), to: storeToInsert)
+
+        expect(storeToLoad, toRetrieve: .found(feed: feed, timestamp: timestamp))
     }
     
     func test_insert_overridesFeedInsertedOnAnotherInstance() {
-//        let storeToInsert = makeSUT()
-//        let storeToOverride = makeSUT()
-//        let storeToLoad = makeSUT()
-//
-//        insert((uniqueImageFeed(), Date()), to: storeToInsert)
-//
-//        let latestFeed = uniqueImageFeed()
-//        let latestTimestamp = Date()
-//        insert((latestFeed, latestTimestamp), to: storeToOverride)
-//
-//        expect(storeToLoad, toRetrieve: .found(feed: latestFeed, timestamp: latestTimestamp))
+        let storeToInsert = makeSUT()
+        let storeToOverride = makeSUT()
+        let storeToLoad = makeSUT()
+
+        insert((uniqueImageFeed(), Date()), to: storeToInsert)
+
+        let latestFeed = uniqueImageFeed()
+        let latestTimestamp = Date()
+        insert((latestFeed, latestTimestamp), to: storeToOverride)
+
+        expect(storeToLoad, toRetrieve: .found(feed: latestFeed, timestamp: latestTimestamp))
     }
     
     func test_delete_deletesFeedInsertedOnAnotherInstance() {
-//        let storeToInsert = makeSUT()
-//        let storeToDelete = makeSUT()
-//        let storeToLoad = makeSUT()
-//
-//        insert((uniqueImageFeed(), Date()), to: storeToInsert)
-//
-//        deleteCache(from: storeToDelete)
-//
-//        expect(storeToLoad, toRetrieve: .empty)
+        let storeToInsert = makeSUT()
+        let storeToDelete = makeSUT()
+        let storeToLoad = makeSUT()
+
+        insert((uniqueImageFeed(), Date()), to: storeToInsert)
+
+        deleteCache(from: storeToDelete)
+
+        expect(storeToLoad, toRetrieve: .empty)
     }
     
     // - MARK: Helpers
     
     private func makeSUT() -> FeedStore {
-        fatalError("Must be implemented")
+      let sut = RealmFeedStore {
+        let realm = try! RealmSwift.Realm(configuration: self.integrationTestRealmConfiguration())
+        return realm
+      }
+      
+      return sut
     }
     
+    private func integrationTestRealmConfiguration() -> RealmSwift.Realm.Configuration {
+      return RealmSwift.Realm.Configuration(fileURL: realmURLForIntegrationTest())
+    }
+  
     private func setupEmptyStoreState() {
-
+      try? FileManager.default.removeItem(at: realmURLForIntegrationTest())
     }
 
     private func undoStoreSideEffects() {
-
+      try? FileManager.default.removeItem(at: realmURLForIntegrationTest())
+    }
+  
+    private func realmURLForIntegrationTest() -> URL {
+      let cachePath = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
+      return cachePath.appendingPathComponent("feedStoreIntegrationTest.realm")
     }
     
 }
